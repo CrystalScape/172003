f9fa9e056c1dbda438bb892dcb1e2b4768894ac3
YUDO NIDLOM FIRMANSYAH <113111545+CrystalScape@users.noreply.github.com>
2024-09-16 21:48:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+st.set_page_config('17-sep-2003💕' , '🎂')
+st.title('SAYANG KUU CINTA KUUU SELAMAT ULANG TAHUNNNNNN SAYANGGGGG 💕🥰 MUACHHH ')
+st.write('SEMOGA TAMBAH SEHAT TAMBAH NURUT SAMA AKU TAMBAH CANTIKK HEHE SAYANGGG KUUU CINTA KUUU')
+st.header('Ini kamu play yaaa sayangg kuuu')
+st.audio("0916(1).MP3", format="audio/mpeg", loop=True)
+VIDEO_URL = "https://drive.google.com/file/d/1irC8-R6UDPMbO0EZuB6qS00Qi-97BOoW/view?usp=sharing"
+st.video(VIDEO_URL)
+st.balloons()
